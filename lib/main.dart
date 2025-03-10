ffa8dde2c1a227e40c09afd95244f9ac37019845
Gimmy <gm0621@gmail.com>
2025-03-10 22:34:34 +0800

    Flutter Lesson in THU by Gimmy

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        body: SafeArea(
-          child: Container(
-            child: Center(
-              child: Text('My Love', style: TextStyle(fontSize: 50.0)),
-            ),
-          ),
-        ),
-      ),
+      home: Scaffold(backgroundColor: Colors.teal, body: Container()),
     );
   }
 }
